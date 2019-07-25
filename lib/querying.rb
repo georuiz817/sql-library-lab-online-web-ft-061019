12f4810eb1b4e043b89b6e92752b61072a08f05c
George Ruiz <georuiz817@gmail.com>
2019-07-25 04:31:15 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT value, count  FROM characters "
+  "SELECT species FROM characters ORDER BY  "
 end
 
 def select_name_and_series_subgenres_of_authors
