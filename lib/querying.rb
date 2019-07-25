b2b68512330f85cf2bd5bb69983cb154b7c2688f
George Ruiz <georuiz817@gmail.com>
2019-07-25 04:32:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species FROM characters ORDER BY  "
+  "SELECT , COUNT(color) FROM bears GROUP BY color ORDER BY count(color) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
