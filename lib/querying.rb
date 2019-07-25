2804eb8b45d5e7e10c11280d1815ee67b4769b00
George Ruiz <georuiz817@gmail.com>
2019-07-25 04:46:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM series WHERe"
+   "SELECT Series.title 
+    FROM characters INNER JOIN series
+    ON characters.series_id = series.id
+    GROUP BY Series.title, species
+    HAVING characters.species = 'human'
+    ORDER BY COUNT(species) DESC
+    LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
